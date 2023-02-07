a0aa88f248f1e40967d5a48e98d57050a6ef1cfb
Юрий Лисов <yulisov@fil-it.ru>
2023-02-07 19:38:39 +0300

    2-14 Sidebar. Layout приложения

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {classNames} from "shared/lib/classNames/classNames";
 import cls from './Navbar.module.scss'
 import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
-import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
 
 // тип для дополнительных пропсов, которые можно докидывать из вне Navbar
 interface NavbarProps {
@@ -12,7 +11,6 @@ interface NavbarProps {
 export const Navbar = ({className}: NavbarProps) => {
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
-            <ThemeSwitcher />
             <div className={cls.links}>
                 <AppLink to='/' className={cls.mainLink} theme={AppLinkTheme.SECONDARY}>Главная</AppLink>
                 <AppLink to='/about' theme={AppLinkTheme.SECONDARY}>О сайте</AppLink>
