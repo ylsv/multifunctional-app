58a70302eb89794e0d717ab32416c5c594875b2b
Юрий Лисов <yulisov@fil-it.ru>
2023-05-15 15:05:26 +0300

    11-71 HTML report для тестов

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -113,7 +113,15 @@ export default {
   // projects: undefined,
 
   // Use this configuration option to add custom reporters to Jest
-  // reporters: undefined,
+  reporters: [
+    "default",
+    ["jest-html-reporters", {
+      publicPath: '<rootDir>/reports/unit',
+      filename: "report.html",
+      openReport: true,
+      inlineSource: true
+    }]
+  ],
 
   // Automatically reset mock state before every test
   // resetMocks: false,
